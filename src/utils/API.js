30660a80109d7ab300c47b229b4d79967c66c68a
Brandy Quinlan <brandyquinlan.bootcamp@gmail.com>
2021-04-27 09:48:10 -0500

    add eslint comment

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
- export default {
-    getEmployees : function(){
-      return axios.get("https://randomuser.me/api/?results=100&nat=us")
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    getEmployees: function() {
+      return axios.get('https://randomuser.me/api/?results=40');
     }
- } ;
+  };
