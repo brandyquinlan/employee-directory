2369353f3c294e980249cf066d2c3bf8bf243f78
Brandy Quinlan <brandyquinlan.bootcamp@gmail.com>
2021-04-26 21:47:54 -0500

    add table column headers

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,58 +1,70 @@
-import React, { Component } from 'react';
-import Employees from './Employees';
-import SearchForm from './SearchForm';
-import moment from "moment";
-import API from '../utils/API';
+import React, { Component } from 'react'
+import "./style.css"
+import Employees from './Employees'
+import Filter from './Filter'
+import moment from "moment"
+import API from '../utils/API'
 
 export default class Container extends Component {
     state = {
-        search: '',
+        filter: '',
         employees: [],
         filteredEmployees: [],
-        sorted: false
+        filtered: false
     };
 
     componentDidMount = () => {
         API.getEmployees()
             .then(({ data }) => {
-                this.setState({ employees: data.results });
+                this.setState({ employees: data.results })
             })
-            .catch((err) => console.log(err));
     };
-    // handleSearchTerm = e =>  {
-    //     setSearchTerm(e.target.value)
-    // }
+
+    sortLastName = () => {
+		const sortedEmployees = this.state.employees
+		sortedEmployees.sort((a, b) => (a.name.last > b.name.last ? 1 : -1))
+		this.setState({ employees: sortedEmployees, filtered:false})
+	};
     
     handleInputChange = e => {
         const value = e.target.value;
         const name = e.target.name;
         console.log(name)
         console.log(value)
-        this.setState({ search: e.target.value });
+        this.setState({ filter: e.target.value });
     };
     
-
     getEmployees = (e) => {
-        const { employees, search } = this.state;
-        this.setState({ search: e.target.value });
+        const { employees, filter } = this.state;
+        this.setState({ filter: e.target.value })
 
         const filteredEmployees = employees.filter(
             (employee) =>
-                employee.name.first.toLowerCase().indexOf(search.toLowerCase()) > -1 ||
-                employee.name.last.toLowerCase().indexOf(search.toLowerCase()) > -1
+                employee.name.last.toLowerCase().indexOf(filter.toLowerCase()) > -1
         );
 
-        this.setState({ filteredEmployees: filteredEmployees, sorted: true });
+        this.setState({ filteredEmployees: filteredEmployees, filtered: true });
     };
 
     render() {
         return (
-            <div>
-                <SearchForm
-                    search={this.search} handleInputChange={this.handleInputChange}
-                />
-                <div className='container-fluid mt-5'>
+            <div className='container-fluid mt-5'>
+                {/* <Filter
+                    newFilter={this.getEmployees} name
+                /> */}
+                <Filter newFilter={this.getEmployees} name='filter' />
+               
                     <table className='table table-striped'>
+                    <thead>
+							<tr>
+								<th></th>
+								<th>First Name </th>
+								<th className='dropdown-toggle' onClick={this.sortLastName}>Last Name</th>
+								<th>Email </th>
+								<th>Phone number</th>
+								<th>DOB </th>
+							</tr>
+						</thead>
                         <tbody>
                             {(
                                 this.state.employees.map((employee, i) => (
@@ -64,7 +76,7 @@ export default class Container extends Component {
                                         email={employee.email}
                                         phone={employee.phone}
                                         dob={moment(employee.dob.date, "YYYY MM DD").format(
-                                            "MMMM D YYYY"
+                                            "MMMM D, YYYY"
                                         )}
                                     />
                                 ))
@@ -72,8 +84,8 @@ export default class Container extends Component {
                         </tbody>
                     </table>
                 </div>
-            </div>
-        );
+            // </div>
+        )
     }
 }
 
