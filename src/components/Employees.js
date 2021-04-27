0ccd4a3f32f589e49d89765c8762ac6ebec6b6b5
Brandy Quinlan <brandyquinlan.bootcamp@gmail.com>
2021-04-26 19:11:17 -0500

    reordeer table

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -7,9 +7,9 @@ function Employees(props) {
             <td><img alt= {props.firstName} src= {props.image} className="img-fluid"/></td>
             <td>{props.firstName}</td>
             <td>{props.lastName}</td>
-            <td>{props.dob}</td>
             <td>{props.email}</td>
             <td>{props.phone}</td>
+            <td>{props.dob}</td>
             </tr>
         </>
     )
