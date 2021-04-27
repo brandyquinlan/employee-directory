b250beaf11dcaf9060acc338348ddd156c566e2c
Brandy Quinlan <brandyquinlan.bootcamp@gmail.com>
2021-04-27 09:47:38 -0500

    add search bar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './SearchBar.css';
+
+class SearchBar extends Component {
+    __onInput = (e) => {
+        const { onInput } = this.props;
+        e.preventDefault();
+
+        if (onInput) {
+            onInput(e.target.value);
+        }
+    }
+
+    render() {
+        return (
+            <div className="search-bar-container">
+            <div className="search-bar-title">
+                <h2>Search By Title </h2>
+            </div>
+                <input
+                    type="text"
+                    onInput={(e) => this.__onInput(e)}
+                />
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
