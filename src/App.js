d28f7157eae7ff09ea122008416237d096947098
Brandy Quinlan <brandyquinlan.bootcamp@gmail.com>
2021-04-26 21:47:23 -0500

    remove two components from app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
 import Container from './components/Container'
-import Employees from './components/Employees'
-import SearchForm from './components/SearchForm'
 
 function App() {
   return (
