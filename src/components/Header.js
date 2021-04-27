d53ae68de07d936c1995f5c3796468dc89c35e23
Brandy Quinlan <brandyquinlan.bootcamp@gmail.com>
2021-04-26 21:48:48 -0500

    update header with style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-function header() {
+export default function header() {
 	return (
-		<div className='jumbotron'>
-			EMPLOYEE-DIRECTORY
-		</div>
-	);
+		<div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">Employee Directory</h1>
+          <p className="lead">Sort or Search by Last Name</p>
+        </div>
+      </div>
+	)
 }
 
-export default header;
